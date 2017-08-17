e4abc810b1b1bad60df3013df55cd34d5a3db3db
Tijmen de Graaff <tijmen76@gmail.com>
2017-08-17 16:37:16 +0200

    render post to post component

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,9 +1,38 @@
 import React, { Component } from "react";
+import { fetchPost } from "../actions";
+import { connect } from "react-redux";
 
 class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
   render() {
-    return <div>PostShow!</div>;
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <h3>
+          {post.title}
+        </h3>
+        <h6>
+          Categories: {post.categories}
+        </h6>
+        <p>
+          {post.content}
+        </p>
+      </div>
+    );
   }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
